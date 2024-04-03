4c8ccee436af0f880c5d85e51179c3f3269e9bd0
fabriciovieira <Bibito23@>
2024-04-03 02:11:15 -0300

    update event controller

--- pass-in/src/main/java/com/rocketseat/passin/controllers/EventController.java
+++ pass-in/src/main/java/com/rocketseat/passin/controllers/EventController.java
@@ -1,15 +1,22 @@
 package com.rocketseat.passin.controllers;
 
+import com.rocketseat.passin.service.EventService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
-    @GetMapping
-    public ResponseEntity<String> getText() {
+
+    private final EventService service;
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getEvent(@PathVariable String id) {
+        this.service.getEventDetail(id);
         return ResponseEntity.ok("Sucess");
     }
 }
