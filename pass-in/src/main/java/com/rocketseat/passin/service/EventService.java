9ea1b3f36a40ecbf23656a82461af8a559eea039
fabriciovieira <Bibito23@>
2024-04-04 00:44:59 -0300

    created event service

--- pass-in/src/main/java/com/rocketseat/passin/service/EventService.java
+++ pass-in/src/main/java/com/rocketseat/passin/service/EventService.java
@@ -1,18 +1,48 @@
 package com.rocketseat.passin.service;
 
+import com.rocketseat.passin.domain.attendee.Attendee;
 import com.rocketseat.passin.domain.event.Event;
+import com.rocketseat.passin.domain.event.exceptions.EventNotFoundException;
+import com.rocketseat.passin.dto.event.EventIdDto;
+import com.rocketseat.passin.dto.event.EventRequestDto;
+import com.rocketseat.passin.dto.event.EventResponseDto;
 import com.rocketseat.passin.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+    private final AttendeeService attendeeService;
 
-    public void getEventDetail(String eventId) {
+    public EventResponseDto getEventDetail(String eventId) {
         Event event = this.eventRepository.findById(eventId).orElseThrow(() ->
-                new RuntimeException("Event not found with ID: " + eventId));
-        return;
+                new EventNotFoundException("Event not found with ID: " + eventId));
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
+        return new EventResponseDto(event, attendeeList.size());
+    }
+
+    public EventIdDto createEvent(EventRequestDto eventDto) {
+        Event newEvent = new Event();
+        newEvent.setTitle(eventDto.title());
+        newEvent.setDetails(eventDto.details());
+        newEvent.setMaximumAttendees(eventDto.maximumAttendees());
+        newEvent.setSlug(this.createSlug(eventDto.title()));
+
+        this.eventRepository.save(newEvent);
+
+        return new EventIdDto(newEvent.getId());
+    }
+
+    private String createSlug(String text) {
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
     }
 }
