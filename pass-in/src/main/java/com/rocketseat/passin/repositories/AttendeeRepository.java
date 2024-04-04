7ae716d59de85d734edc459508448965de03ce52
fabriciovieira <Bibito23@>
2024-04-04 00:41:16 -0300

    create attendee repository

--- pass-in/src/main/java/com/rocketseat/passin/repositories/AttendeeRepository.java
+++ pass-in/src/main/java/com/rocketseat/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,8 @@ package com.rocketseat.passin.repositories;
 import com.rocketseat.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    List<Attendee> findByEventId(String eventId);
 }
