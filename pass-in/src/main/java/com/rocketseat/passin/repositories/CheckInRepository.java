ae6d62eef3154122cf4b8f1622709026a4cb89f6
fabriciovieira <Bibito23@>
2024-04-04 00:42:42 -0300

    updated checkin repository

--- pass-in/src/main/java/com/rocketseat/passin/repositories/CheckInRepository.java
+++ pass-in/src/main/java/com/rocketseat/passin/repositories/CheckInRepository.java
@@ -3,5 +3,8 @@ package com.rocketseat.passin.repositories;
 import com.rocketseat.passin.domain.checkin.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
