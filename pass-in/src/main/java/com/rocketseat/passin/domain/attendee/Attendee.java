28c0f35c93370915c212e458f93977642bc31c04
fabriciovieira <Bibito23@>
2024-04-04 00:40:26 -0300

    update attendee class

--- pass-in/src/main/java/com/rocketseat/passin/domain/attendee/Attendee.java
+++ pass-in/src/main/java/com/rocketseat/passin/domain/attendee/Attendee.java
@@ -28,4 +28,44 @@ public class Attendee {
     private Event event;
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
